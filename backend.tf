757ab5d588b062be56dd01e8ca9f644b20c3dcab
Khadijah Celestine <khadijah.celestine@gmail.com>
2025-02-23 21:34:05 -0500

    add s3 backend with dynamodb for locking

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {                                 
+    region         = "us-east-1"                
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+    acl            = "bucket-owner-full-control"
+    bucket         = "khadijah-flask-app-state"
+    key            = "dev/terraform.tfstate"
+  }
+}

ff57755ecf3614c5a1d39f74791705721a4ba96d
Khadijah Celestine <khadijah.celestine@gmail.com>
2025-02-23 22:58:41 -0500

    removed nat-gateway module, put in vpc module; added vpc-flow-log module

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -39,8 +39,6 @@ resource "aws_subnet" "private" {
     }
 }
 
-
-
 resource "aws_internet_gateway" "this" {
   vpc_id = aws_vpc.this.id
 
@@ -60,7 +58,6 @@ resource "aws_nat_gateway" "this" {
   subnet_id     = aws_subnet.private[each.key].id
   allocation_id = aws_eip.public[each.key].id
 }
-
 
 resource "aws_route_table" "private" {
     for_each          = aws_nat_gateway.this
@@ -101,76 +98,4 @@ resource "aws_route_table_association" "public" {
     route_table_id = aws_route_table.public.id
 }
 
-
-# # ### VPC Flow Logs
-# # resource "aws_s3_bucket" "vpc_flow_logs" {
-# #   bucket = var.s3_flow_logs
-# # }
-
-# # resource "aws_flow_log" "vpc_flow_log" {
-# #     #count = "${var.enable_vpc_flow_log}"
-# #     iam_role_arn    = aws_iam_role.vpc_flow_logs.arn
-# #     log_destination      = aws_s3_bucket.vpc_flow_logs.arn
-# #     log_destination_type = "s3"
-# #     traffic_type         = "ALL"
-# #     vpc_id               = aws_vpc.this.id
-
-# # }
-
-# # resource "aws_iam_role" "vpc_flow_logs" {
-# #   name = "enable_vpc_flow_log"
-# #   #count = "${var.enable_vpc_flow_log}"
-# #   assume_role_policy = <<EOF
-# # {
-# #   "Version": "2012-10-17",
-# #   "Statement": [
-# #     {
-# #       "Sid": "",
-# #       "Effect": "Allow",
-# #       "Principal": {
-# #         "Service": "vpc-flow-logs.amazonaws.com"
-# #       },
-# #       "Action": "sts:AssumeRole"
-# #     }
-# #   ]
-# # }
-# # EOF
-# # }
-# # resource "aws_iam_role_policy" "example" {
-# #   name = "enable_vpc_flow_log"
-# #   #count = "${var.enable_vpc_flow_log}"
-# #   role = "${aws_iam_role.vpc_flow_logs.id}"
-# #   policy = <<EOF
-# # {
-# #   "Version": "2012-10-17",
-# #   "Statement": [
-# #     {
-# #       "Action": [
-# #         "logs:CreateLogGroup",
-# #         "logs:CreateLogStream",
-# #         "logs:PutLogEvents",
-# #         "logs:DescribeLogGroups",
-# #         "logs:DescribeLogStreams"
-# #       ],
-# #       "Effect": "Allow",
-# #       "Resource": "*"
-# #     }
-# #   ]
-# # }
-# # EOF
-# # }s
-
-
-# resource "aws_flow_log" "this" {
-#   log_destination      = aws_s3_bucket.vpc_flow_log.arn
-#   log_destination_type = "s3"
-#   traffic_type         = "ALL"
-#   vpc_id               = aws_vpc.this.id
-# }
-
-# resource "aws_s3_bucket" "vpc_flow_log" {
-#   bucket = var.s3_flow_logs
-
-#   force_destroy = true
-# }
 

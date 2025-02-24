286d2b7606639c8cec3019d6cf929c6db9d6f64a
Khadijah Celestine <khadijah.celestine@gmail.com>
2025-02-24 11:50:10 -0500

    added vpc-flow-logs module to main

--- main.tf
+++ main.tf
@@ -6,18 +6,25 @@ module "flask-vpc" {
   public_tag   = var.public_tag
   private_tag  = var.private_tag
   s3_flow_logs = var.s3_flow_logs
+  #remove later
+  ec2_instance_type = var.ec2_instance_type
+  ec2_security_group_name = var.ec2_security_group_name
+  ec2_security_group_description = var.ec2_security_group_description
 }
 
 module "flask-ec2" {
   source = "./modules/ec2"
-}
-
-module "flask-nat-gateway" {
-  source      = "./modules/nat-gateway"
+  ec2_security_group_name = var.ec2_security_group_name
+  ec2_security_group_description = var.ec2_security_group_description
+  ec2_instance_type = var.ec2_instance_type
+  aws_ec2_key = "wp"
   public_tag  = var.public_tag
   private_tag = var.private_tag
+  vpc_id       = module.flask-vpc.vpc_id
 }
 
-
-
+module "flask-vpc-flow-logs" {
+  source = "./modules/vpc-flow-logs"
+  s3_flow_logs = var.s3_flow_logs
+}
 # permanently delete

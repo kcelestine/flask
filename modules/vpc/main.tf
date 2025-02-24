c21192c714c20f3a6e5a16bbd47defaf6e1b2375
Khadijah Celestine <khadijah.celestine@gmail.com>
2025-02-23 21:51:01 -0500

    added nat-gateway module

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -57,38 +57,6 @@ resource "aws_vpc" "this" {
 # }
 
 
-
-# ##### NAT Gateways #####
-# resource "aws_eip" "public_1a" {
-#   depends_on = [aws_internet_gateway.this]
-# }
-
-# resource "aws_eip" "public_1b" {
-#   depends_on = [aws_internet_gateway.this]
-# }
-
-# resource "aws_nat_gateway" "public_1a" {
-#   allocation_id = aws_eip.public_1a.id
-#   subnet_id     = aws_subnet.public_us_east_1a.id
-
-#   tags = {
-#     Name = var.public_tag
-#   }
-
-#   depends_on = [aws_internet_gateway.this]
-# }
-
-# resource "aws_nat_gateway" "public_1b" {
-#   allocation_id = aws_eip.public_1b.id
-#   subnet_id     = aws_subnet.public_us_east_1b.id
-
-#   tags = {
-#     Name = var.private_tag
-#   }
-
-#   depends_on = [aws_internet_gateway.this]
-# }
-
 # resource "aws_route_table" "public" {
 #   vpc_id = aws_vpc.this.id
 

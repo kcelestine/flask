bef7f10601ecbd430c77733722475c8900d178a7
Khadijah Celestine <khadijah.celestine@gmail.com>
2025-02-24 11:49:31 -0500

    added outputs to vpc and ec2 modules

---
+++ modules/ec2/outputs.tf
@@ -0,0 +1,3 @@
+# output "ec2_global_ips" {
+#   value = [for instance in aws_instance.this : instance.public_ip]
+# }
---
+++ modules/vpc/outputs.tf
@@ -0,0 +1,10 @@
+output "vpc_id" {
+  value = aws_vpc.this.id
+}
+
+# output "public_subnets" {
+#   value = 
+# }
+# output "private_subnets" {
+#   value = 
+# }

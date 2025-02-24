db7a61010a7c551dc56c473f0c3eb4b183f6d260
Khadijah Celestine <khadijah.celestine@gmail.com>
2025-02-23 21:34:59 -0500

    add version file to specify versions for terraform and aws

---
+++ version.tf
@@ -0,0 +1,20 @@
+terraform {
+
+  required_version = "1.10.1"
+
+  required_providers {
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.86.1"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+
+
+
+}

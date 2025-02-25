1570762f5ea0ad79f1e97abea605d359573af121
Khadijah Celestine <khadijah.celestine@gmail.com>
2025-02-24 22:36:21 -0500

    updated format

--- main.tf
+++ main.tf
@@ -9,22 +9,22 @@ module "flask-vpc" {
 }
 
 module "flask-ec2" {
-    source = "./modules/ec2"
-    ec2_security_group_name = var.ec2_security_group_name
-    ec2_security_group_description = var.ec2_security_group_description
-    ec2_instance_type = var.ec2_instance_type
-    aws_ec2_key = "wp"
-    public_tag  = var.public_tag
-    private_tag = var.private_tag
-    #from vpc module
-    vpc_id       = module.flask-vpc.vpc_id
-    public_subnet_ids = module.flask-vpc.public_subnet_ids
-    private_subnet_ids = module.flask-vpc.private_subnet_ids
+  source                         = "./modules/ec2"
+  ec2_security_group_name        = var.ec2_security_group_name
+  ec2_security_group_description = var.ec2_security_group_description
+  ec2_instance_type              = var.ec2_instance_type
+  aws_ec2_key                    = "wp"
+  public_tag                     = var.public_tag
+  private_tag                    = var.private_tag
+  #from vpc module
+  vpc_id             = module.flask-vpc.vpc_id
+  public_subnet_ids  = module.flask-vpc.public_subnet_ids
+  private_subnet_ids = module.flask-vpc.private_subnet_ids
 }
 
 module "flask-vpc-flow-logs" {
-  source = "./modules/vpc-flow-logs"
+  source       = "./modules/vpc-flow-logs"
   s3_flow_logs = var.s3_flow_logs
-  vpc_id      = module.flask-vpc.vpc_id
+  vpc_id       = module.flask-vpc.vpc_id
 }
 # permanently delete

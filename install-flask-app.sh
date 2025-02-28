d112c336d4c169759387214abf2754a0b242c39c
Khadijah Celestine <khadijah.celestine@gmail.com>
2025-02-28 00:29:42 -0500

    successfully runs but still trying to run flask app in background

--- install-flask-app.sh
+++ install-flask-app.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Update the system and install required dependencies
-apt-get update -y
-apt-get install -y build-essential gcc make zlib1g-dev libbz2-dev \
-libssl-dev libncurses5-dev libsqlite3-dev readline-dev tk-dev \
+sudo apt-get update -y
+sudo apt-get install -y build-essential gcc make zlib1g-dev libbz2-dev \
+libssl-dev libncurses5-dev libsqlite3-dev libreadline-dev tk-dev \
 libgdbm-dev libnss3-dev libffi-dev git
 
 # Install Python 2.7.18 from source
@@ -12,23 +12,29 @@ wget https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz
 tar -xvzf Python-2.7.18.tgz
 cd Python-2.7.18
 ./configure --prefix=/usr/local
-make
-make install
+sudo make
+sudo make install
 
-# # Check Python version to verify installation
-# /usr/local/bin/python2.7 --version
+# Check Python version to verify installation
+/usr/local/bin/python2.7 --version
 
-# # Set up virtual environment with Python 2.7
-# cd /home/ubuntu
-# git clone https://github.com/your-username/your-flask-repo.git
-# cd your-flask-repo
+# Set up virtual environment with Python 2.7
+cd /home/ubuntu
+git clone https://github.com/kcelestine/flask-app.git
+cd flask-app
 
-# # Create a virtual environment with Python 2.7
-# /usr/local/bin/python2.7 -m virtualenv venv
-# source venv/bin/activate
+# get pip
+wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
+sudo python2.7 get-pip.py
+sudo pip2.7 install virtualenv
+#pip2.7 --version
 
-# # Install dependencies from requirements.txt
-# pip install -r requirements.txt
+# Create a virtual environment with Python 2.7
+/usr/local/bin/python2.7 -m virtualenv venv
+source venv/bin/activate
+
+# Install dependencies from requirements.txt
+pip install -r requirements.txt
 
 # # Set environment variables for Flask app
 # export FLASK_APP=app.py
@@ -36,3 +42,15 @@ make install
 
 # # Start the Flask app (use gunicorn or flask run for production use)
 # flask run --host=0.0.0.0 --port=80
+
+# create the database
+python db_create.py
+
+# start flask
+#python application.py &
+
+# Run the Python script in the background
+nohup python /home/ubuntu/flask-app/application.py > /home/ubuntu/flask-app/script.log 2>&1 &
+
+# Check if the Python script is running using ps aux (optional logging)
+ps aux | grep application.py >> /home/ubuntu/flask-app/script_status.log
